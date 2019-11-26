7afb42d9aa26ea798b86b907f61b6e9a5088671e
Marco Matildo <marcomatildo@gmail.com>
2019-11-27 02:21:12 +0800

    :sparkles: add material design to an existing codebase

--- mdc-111/starter/app.js
+++ mdc-111/starter/app.js
@@ -1,5 +1,18 @@
+import { MDCRipple } from '@material/ripple';
+import { MDCSelect } from '@material/select';
+import { MDCTextField } from '@material/textfield';
+
 const shippingForm = document.querySelector('#crane-shipping-form');
 shippingForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   alert('Success!');
+});
+// button ripple
+new MDCRipple(document.querySelector('.mdc-button'));
+// select 
+new MDCSelect(document.querySelector('.mdc-select'));
+// allText-field to have material theming
+const textFieldElements = [].slice.call(document.querySelectorAll('.mdc-text-field'));
+textFieldElements.forEach((textFieldEl) => {
+  new MDCTextField(textFieldEl);
 });
